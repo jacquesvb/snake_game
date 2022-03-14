766a7e5ecad2b64e893e9305e0a3ef2a0c4855f7
Jacques Van Blokland <jacquesvb@live.com>
2022-03-14 00:04:47 -0700

    Modules

--- main.rs
+++ main.rs
@@ -1,65 +1,8 @@
-
-trait Log {
-    fn display_info(&self);
-    fn alert_something(&self) {
-        println!("Default implementation!!!")
-    } 
-}
-
-#[derive(Debug)]
-enum PersonId {
-    Passport(u32),
-    IdentityCard(u32, u32, u32),
-}
-
-struct Person {
-    name: String, // fields
-    last_name: String,
-    age: u32,
-    id: PersonId,
-}
-
-struct Animal(String);
-
-impl Log for Animal {
-    fn display_info(&self) {
-        println!("{}", self.0)
-    }
-
-    fn alert_something(&self) {
-        println!("ANIMAL implementation!!!!!!!")
-    }
-}
-
-impl Log for Person {
-    fn display_info(&self) {
-        println!("{} {} {} {:?}", self.name, self.last_name, self.age, self.id)
-    }
-}
-
-impl Person {
-    fn new() -> Person {
-        Person {
-            name: "Default".to_string(),
-            last_name: "Default".to_string(),
-            age: 0,
-            id: PersonId::IdentityCard(820, 18, 9435)
-        }
-    }
-
-    fn from(name: String, last_name: String, age: u32, id: PersonId) -> Person {
-        Person {
-            name,
-            last_name,
-            age,
-            id
-        }
-    }
-
-    fn change_age(&mut self, new_age: u32) {
-        self.age = new_age;
-    }
-}
+// use snake_game::Person;
+// use snake_game::Animal;
+// use snake_game::log_info;
+// use snake_game::log_info_2;
+use snake_game::learning_rust::{Person, Animal, log_info, log_info_2};
 
 fn main() {
     let mut person = Person::new();
@@ -69,17 +12,4 @@ fn main() {
 
     log_info(person);
     log_info_2(&animal);
-}
-
-// impl makes the compiler determine type at the compile time
-// it will create multiple versions of the function, depending on
-// how many types Log trait implements (Person, Animal)
-fn log_info(val: impl Log) {
-    val.alert_something();
-}
-
-// dyn is short for dynamic, an says that function should perform dynamic dispatch
-// decision of exatly which function to call at the runtime
-fn log_info_2(val: &dyn Log) {
-    val.alert_something();
 }
