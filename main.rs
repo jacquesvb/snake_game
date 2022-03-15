6c7a0b1ee097a669a9c6e78b664a5775743b112c
Jacques Van Blokland <jacquesvb@live.com>
2022-03-14 22:02:21 -0700

    External lib

--- main.rs
+++ main.rs
@@ -1,15 +1,8 @@
-// use snake_game::Person;
-// use snake_game::Animal;
-// use snake_game::log_info;
-// use snake_game::log_info_2;
-use snake_game::learning_rust::{Person, Animal, log_info, log_info_2};
+
+
+use snake_game::education::learning_rust::{Person};
 
 fn main() {
-    let mut person = Person::new();
-    let animal = Animal(String::from("dog"));
-
-    person.change_age(38);
-
-    log_info(person);
-    log_info_2(&animal);
+    let person = Person::new();
+    println!("{}", person.id)
 }
