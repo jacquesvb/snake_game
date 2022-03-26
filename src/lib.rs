3813b77ed361b201eb5c93e0c24885c6dd34c6dd
Jacques Van Blokland <jacquesvb@live.com>
2022-03-26 15:42:03 -0700

    Update the World

--- src/lib.rs
+++ src/lib.rs
@@ -4,41 +4,88 @@ use wee_alloc::WeeAlloc;
 #[global_allocator]
 static ALLOC: WeeAlloc = WeeAlloc::INIT;
 
+#[derive(PartialEq)]
+enum Direction {
+  Up,
+  Right,
+  Down,
+  Left,
+}
+
 struct SnakeCell(usize);
 
 struct Snake {
-  body: Vec<SnakeCell>
+    body: Vec<SnakeCell>,
+    direction: Direction,
 }
 
 impl Snake {
-  fn new(spawn_index: usize) -> Snake {
-    Snake {
-      body: vec!(SnakeCell(spawn_index))
+    fn new(spawn_index: usize) -> Snake {
+        Snake {
+            body: vec!(SnakeCell(spawn_index)),
+            direction: Direction::Up,
+        }
     }
-  }
 }
 
 #[wasm_bindgen]
 pub struct World {
-  width: usize,
-  snake: Snake,
+    width: usize,
+    size: usize,
+    snake: Snake,
 }
+
 #[wasm_bindgen]
 impl World {
-  pub fn new() -> World {
-    World { 
-      width: 16,
-      snake: Snake::new(10)
+    pub fn new(width: usize, snake_idx: usize) -> World {
+        World {
+            width,
+            size: width * width,
+            snake: Snake::new(snake_idx)
+        }
     }
-  }
 
-  pub fn width(&self) -> usize {
-    self.width
-  }
+    pub fn width(&self) -> usize {
+        self.width
+    }
 
-  pub fn snake_head_idx(&self) -> usize {
-    self.snake.body[0].0
-  }
+    pub fn snake_head_idx(&self) -> usize {
+       self.snake.body[0].0
+    }
+
+    pub fn update(&mut self) {
+        let snake_idx = self.snake_head_idx();
+        let (row, col) = self.index_to_cell(snake_idx);
+        let (row, col) = match self.snake.direction {
+            Direction::Right => {
+                (row, (col + 1) % self.width)
+            },
+            Direction::Left => {
+                (row, (col - 1) % self.width)
+            },
+            Direction::Up => {
+                ((row - 1) % self.width, col)
+            },
+            Direction::Down => {
+                ((row +1) % self.width, col)
+            },
+        };
+
+        let next_idx = self.cell_to_index(row, col);
+        self.set_snake_head(next_idx);
+    }
+
+    fn set_snake_head(&mut self, idx: usize) {
+        self.snake.body[0].0 = idx;
+    }
+
+    fn index_to_cell(&self, idx: usize) -> (usize, usize) {
+        (idx / self.width, idx % self.width)
+    }
+
+    fn cell_to_index(&self, row: usize, col: usize) -> usize {
+        (row * self.width) + col
+    }
 }
 
 // wasm-pack build --target web
--- www/bootstrap.js
+++ www/bootstrap.js
@@ -1,2 +1,2 @@
-import("./index.js")
+import("./index.ts")
   .catch(e => console.error("Error importing index.js :", e))
