d5212946364c7f75f405589576fac166e1a52351
Jacques Van Blokland <j.vanblokland.ext@slingshotaerospace.com>
2022-03-25 22:13:41 -0700

    Starting the Game

--- main.rs
+++
@@ -1,8 +0,0 @@
-
-
-use snake_game::education::learning_rust::{Person};
-
-fn main() {
-    let person = Person::new();
-    println!("{}", person.id)
-}
--- www/index.js
+++ www/index.js
@@ -1,23 +1,44 @@
-async function init() {
+import init, { World } from "snake_game";
 
-  const importObject = {
-    console: {
-      log: () => {
-        console.log("Just logging something!");
-      },
-      error: () => {
-        console.log("I am just error");
-      }
+init().then(_ => {
+  const CELL_SIZE = 20;
+  const world = World.new();
+  const worldWidth = world.width();
+  const canvas = document.getElementById("snake-canvas");
+  const ctx = canvas.getContext("2d");
+  canvas.height = worldWidth * CELL_SIZE;
+  canvas.width = worldWidth * CELL_SIZE;
+
+  function drawWorld() {
+    ctx.beginPath();
+
+    for (let x = 0; x < worldWidth + 1; x++) {
+      ctx.moveTo(CELL_SIZE * x, 0);
+      ctx.lineTo(CELL_SIZE * x, worldWidth * CELL_SIZE);
     }
+
+    for (let y = 0; y < worldWidth + 1; y++) {
+      ctx.moveTo(0, CELL_SIZE * y);
+      ctx.lineTo(worldWidth * CELL_SIZE, CELL_SIZE * y);
+    }
+
+    ctx.stroke();
   }
 
-  const response = await fetch("sum.wasm");
-  const buffer = await response.arrayBuffer();
-  const wasm = await WebAssembly.instantiate(buffer, importObject);
+  function drawSnake() {
+    const snakeIdx = world.snake_head_idx();
+    const col = snakeIdx % worldWidth;
+    const row = Math.floor(snakeIdx / worldWidth);
 
-  const sumFunction = wasm.instance.exports.sum;
-  const result = sumFunction(100, 300);
-  console.log(result);
-}
-
-init();
+    ctx.beginPath();
+    ctx.fillRect(
+      col * CELL_SIZE,
+      row * CELL_SIZE,
+      CELL_SIZE,
+      CELL_SIZE
+    );
+    ctx.stroke();
+  }
+  drawWorld();
+  drawSnake();
+})
